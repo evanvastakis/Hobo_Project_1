981525793c6fb4fbf98d695801c9971d154777ed
Grant <grfood2@yahoo.com>
2025-02-24 20:18:33 -0500

    Protocol class variables

--- P2P/Protocol.java
+++ P2P/Protocol.java
@@ -1,3 +1,15 @@
 public class Protocol {
     
+    private double timeStamp; // Time at which packet was made.
+    private int portNum; // Sender port num
+    private int ip; // Sender ip
+    private int destPortNum; // Destination port num
+    private int destIp; // Destination ip
+    private String name; // Name of node with its ip
+    private byte[] fileList; // List of that node's files
+
+    public Protocol(){
+        
+    }
+
 }
