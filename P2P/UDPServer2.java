387d32b7db024d2c1d21f3832fe464924c3ecdb8
Evan Vastakis <evan.vastakis05@gmail.com>
2025-02-26 16:10:20 -0500

    Implemented HashMap for the updating of the node status

--- P2P/UDPServer2.java
+++ P2P/UDPServer2.java
@@ -3,6 +3,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.HashMap;
 /**
  * 
  * @author cjaiswal
@@ -14,8 +15,9 @@ public class UDPServer2
 {
     final int NODE_COUNT = 6;
     private boolean[] nodeStatus = new boolean[NODE_COUNT];
-    private String[] nodeIPS = new String[NODE_COUNT];
-    private int[] nodeTimes = new int[NODE_COUNT];
+    private HashMap<String, InetAddress> nodeIPS = new HashMap<String, InetAddress>();
+    private String[] IPKeys = {"node1", "node2", "node3", "node4", "node5", "node6"};
+    // private int[] nodeTimes = new int[NODE_COUNT];
 
     private DatagramSocket socket = null;
 
@@ -39,6 +41,9 @@ public class UDPServer2
         {
         	//incoming data buffer
             byte[] incomingData = new byte[1024];
+            for(String i : IPKeys){
+                nodeIPS.put(i, InetAddress.getLocalHost());
+            }
 
             while (true) 
             {
@@ -48,9 +53,20 @@ public class UDPServer2
                 
                 //wait for the packet to arrive and store it in incoming packet
                 socket.receive(incomingPacket);
+
+                // this is meant to fill up the hash map with different IP values
+                for(int i = 0; i < NODE_COUNT; i++){
+                    if(nodeIPS.get(IPKeys[i]).equals(InetAddress.getLocalHost())){
+                        nodeIPS.put(IPKeys[i], incomingPacket.getAddress());
+                        break;
+                    }
+                    System.out.println(nodeIPS.get(IPKeys[i]));
+                }
+
+                // this will be how we will make sure that the node is up or down
                 for(int i = 0; i < NODE_COUNT; i++){
                     // match incoming packet to its status 
-                    if(nodeIPS[i].equals(incomingPacket.getAddress())){
+                    if(nodeIPS.get(IPKeys[i]).equals(incomingPacket.getAddress())){
                         nodeStatus[i] = true;
                     }
                 }
