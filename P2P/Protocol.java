5545c1cbd9ce49ad9c356597b709a35e5c4ba7f5
Grant <grfood2@yahoo.com>
2025-02-28 13:56:46 -0500

    Protocol methods

--- P2P/Protocol.java
+++ P2P/Protocol.java
@@ -1,15 +1,83 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Protocol {
     
     private double timeStamp; // Time at which packet was made.
     private int portNum; // Sender port num
-    private int ip; // Sender ip
+    private String ip; // Sender ip
     private int destPortNum; // Destination port num
-    private int destIp; // Destination ip
+    private String destIp; // Destination ip
     private String name; // Name of node with its ip
-    private byte[] fileList; // List of that node's files
+    private String fileList; // List of that node's files
 
     public Protocol(){
-        
+        // Get ip
+        try {
+            this.ip = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            this.ip = "Unknown IP";
+        }
     }
 
+    // Getters
+
+    public double getTimeStamp() {
+        return timeStamp;
+    }
+
+    public int getPortNum() {
+        return portNum;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public int getDestPortNum() {
+        return destPortNum;
+    }
+
+    public String getDestIp() {
+        return destIp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFileList() {
+        return fileList;
+    }
+
+    // Setters
+
+        public void setTimeStamp(double timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public void setPortNum(int portNum) {
+        this.portNum = portNum;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public void setDestPortNum(int destPortNum) {
+        this.destPortNum = destPortNum;
+    }
+
+    public void setDestIp(String destIp) {
+        this.destIp = destIp;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFileList(String fileList) {
+        this.fileList = fileList;
+    }
 }
